f2a97c29c7683c85cd47be6a837c17e1a954e769
AndreiBoaca <aboaca.saulescu@gmail.com>
2017-11-10 14:47:16 +0200

    first commit

---
+++ REPOTEST/src/test1/class1.java
@@ -0,0 +1,15 @@
+package test1;
+
+/**
+ * @author aboaca
+ *
+ */
+public class class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Wai pulica fara frica");
+	}
+
+}
